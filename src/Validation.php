051d82429ee10da549167a1d5f30faaa106ca5fa
Sergey Donin <sergey.donin@gmail.com>
2021-02-15 23:15:40 +0100

    Add Validation class with tests

--- src/Validation.php
+++ src/Validation.php
@@ -1,10 +1,40 @@
 <?php
-
+declare(strict_types=1);
 
 namespace Dostrog\Larate;
 
+use Money\Currencies\ISOCurrencies;
+use Money\Currency;
+use Illuminate\Support\Facades\Validator;
+use InvalidArgumentException;
 
 class Validation
 {
+    /**
+     * Validate that the currency is supported by the
+     * Exchange Rates API.
+     *
+     * @param string $currencyCode
+     *
+     * @throws InvalidArgumentException
+     */
+    public static function validateCurrencyCode(string $currencyCode): void
+    {
+        $currencies = new ISOCurrencies();
+        $currencies->contains(new Currency($currencyCode));
 
+        $validator = Validator::make([ 'code' => $currencyCode], [
+            'code' => [
+                function ($attribute, $value, $fail) use ($currencies) {
+                    if (! $currencies->contains(new Currency($value))) {
+                        $fail("'{$value}' is not a valid currency code.");
+                    }
+                },
+            ],
+        ]);
+
+        if ($validator->fails()) {
+            throw new InvalidArgumentException($validator->errors()->first());
+        }
+    }
 }
--- tests/Unit/ValidationTest.php
+++ tests/Unit/ValidationTest.php
@@ -4,51 +4,32 @@ namespace Dostrog\Larate\Tests\Unit;
 
 use Dostrog\Larate\CurrencyPair;
 use Dostrog\Larate\Tests\TestCase;
+use Dostrog\Larate\Validation;
 use InvalidArgumentException;
 
-class CurrencyPairTest extends TestCase
+class ValidationTest extends TestCase
 {
     public const BASE_CURRENCY = 'EUR';
     public const QUOTE_CURRENCY = 'USD';
 
     /** @test */
-    public function factory_method_is_correct(): void
-    {
-        $pair = CurrencyPair::createFromString(self::BASE_CURRENCY . '/' . self::QUOTE_CURRENCY);
-        self::assertInstanceOf(CurrencyPair::class, $pair);
-    }
-
-    /** @test */
-    public function constructor_is_correct(): void
-    {
-        $pair = new CurrencyPair(self::BASE_CURRENCY, self::BASE_CURRENCY);
-        self::assertInstanceOf(CurrencyPair::class, $pair);
-    }
-
-    /** @test */
-    public function getters_is_correct(): void
-    {
-        $pair = new CurrencyPair(self::BASE_CURRENCY, self::BASE_CURRENCY);
-        self::assertTrue($pair->isIdentical());
-
-        $pair = new CurrencyPair(self::BASE_CURRENCY, self::QUOTE_CURRENCY);
-        self::assertNotTrue($pair->isIdentical());
-
-        self::assertSame(self::BASE_CURRENCY, $pair->getBaseCurrency());
-        self::assertSame(self::QUOTE_CURRENCY, $pair->getQuoteCurrency());
-    }
-
-    /** @test */
-    public function invalid_argument_exception_is_trow(): void
+    public function currency_code_validation_not_iso4217(): void
     {
         $this->expectException(InvalidArgumentException::class);
-        $pair = CurrencyPair::createFromString(self::BASE_CURRENCY . '+' . self::BASE_CURRENCY);
+        Validation::validateCurrencyCode('foo');
     }
 
     /** @test */
-    public function toString_method_is_correct_implemented(): void
+    public function currency_code_validation_not_empty(): void
     {
-        $pair = new CurrencyPair(self::BASE_CURRENCY, self::BASE_CURRENCY);
-        self::assertSame(self::BASE_CURRENCY . '/' . self::QUOTE_CURRENCY, $pair->__toString());
+        $this->expectException(InvalidArgumentException::class);
+        Validation::validateCurrencyCode('');
+    }
+
+    /** @test */
+    public function currency_code_validation_is_string(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        Validation::validateCurrencyCode('!@#');
     }
 }
