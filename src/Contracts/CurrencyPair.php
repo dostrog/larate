2a7a7c110d765b121812270ed12a0d4052fa4954
Sergey Donin <sergey.donin@gmail.com>
2021-02-15 23:16:41 +0100

    Add contracts for CurrencyPair and ExchangeRate

--- src/Contracts/CurrencyPair.php
+++ src/Contracts/CurrencyPair.php
@@ -1,10 +1,29 @@
 <?php
-
+declare(strict_types=1);
 
 namespace Dostrog\Larate\Contracts;
 
-
 interface CurrencyPair
 {
+    /**
+     * Get the base currency.
+     *
+     * @return string
+     */
+    public function getBaseCurrency(): string;
+
+    /**
+     * Gets the quote currency.
+     *
+     * @return string
+     */
+    public function getQuoteCurrency(): string;
+
+    /**
+     * Checks if the pair is identical.
+     *
+     * @return bool
+     */
+    public function isIdentical(): bool;
 
 }
--- src/Contracts/ExchangeRate.php
+++ src/Contracts/ExchangeRate.php
@@ -1,10 +1,61 @@
 <?php
-
+declare(strict_types=1);
 
 namespace Dostrog\Larate\Contracts;
 
+use DateTimeInterface;
 
 interface ExchangeRate
 {
+    /**
+     * Gets the rate value.
+     *
+     * @return float
+     */
+    public function getValue(): float;
 
+    /**
+     * Gets the date at which this rate was set by provider
+     *
+     * @return DateTimeInterface
+     */
+    public function getDate(): DateTimeInterface;
+
+
+    /**
+     * Gets the name of the provider that returned this rate.
+     *
+     * @return string
+     */
+    public function getProviderName(): string;
+
+    /**
+     * Gets the currency pair.
+     *
+     * @return CurrencyPair
+     */
+    public function getCurrencyPair(): CurrencyPair;
+
+//    /**
+//     * Get all requested currency rates.
+//     *
+//     * @return array
+//     */
+//    public function getRates();
+//
+//    /**
+//     * Get the rate for the given currency.
+//     * Must return null if currency is not found in the result.
+//     *
+//     * @param string $currency
+//     * @return float|null
+//     */
+//    public function getRate($currency);
+//
+//    /**
+//     * Get all requested currency conversions.
+//     *
+//     * @return array
+//     */
+//    public function getConverted();
 }
