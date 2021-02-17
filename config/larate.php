be97d499fa4e3d01853b65f4a74989b803c064ce
Sergey Donin <sergey.donin@gmail.com>
2021-02-17 19:46:23 +0100

    Add default config params

--- config/larate.php
+++ config/larate.php
@@ -1,5 +1,9 @@
 <?php
 
 return [
-
+    'default_base_currency' => 'RUB',
+    'service' => [
+        'RUB' => \Dostrog\Larate\Services\RussianCentralBank::class,
+        'UAH' => \Dostrog\Larate\Services\NationalBankOfUkraine::class,
+    ],
 ];
