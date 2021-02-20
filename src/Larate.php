faafe78b80a7d038a9ee6c56953b6531b9b73997
Sergey Donin <sergey.donin@gmail.com>
2021-02-20 12:58:46 +0100

    Tests last changes

--- src/Larate.php
+++ src/Larate.php
@@ -3,43 +3,45 @@ declare(strict_types=1);
 
 namespace Dostrog\Larate;
 
-use Cache;
 use DateTimeInterface;
 use Dostrog\Larate\Contracts\CurrencyPair as CurrencyPairContract;
 use Dostrog\Larate\Contracts\ExchangeRate as ExchangeRateContract;
 use Dostrog\Larate\Contracts\ExchangeRateProvider as ExchangeRateProviderContract;
 use Dostrog\Larate\Contracts\ExchangeRateService as ExchangeRateServiceContract;
 use Illuminate\Support\Carbon;
+use Psr\SimpleCache\CacheInterface;
 
 class Larate implements ExchangeRateProviderContract
 {
     /**
-     * The service.
+     * The service where converted rate are
      */
     private ExchangeRateServiceContract $service;
 
-    public static function createForBaseCurrency(string $baseCurrency = 'RUB'): Larate
+    /**
+     * The cache
+     */
+    private CacheInterface $cache;
+
+    public static function createForBaseCurrency(string $baseCurrency = 'RUB', CacheInterface $cache = null): Larate
     {
-        if (isset(config('larate.service')[$baseCurrency])
-            && class_exists(config('larate.service')[$baseCurrency])) {
-            $serviceClass = config('larate.service.' . $baseCurrency);
+        $serviceClass = isset(config('larate.service')[$baseCurrency]) && class_exists(config('larate.service')[$baseCurrency])
+            ? config('larate.service.' . $baseCurrency)
+            : config('larate.service.RUB');
 
-            return new self(new $serviceClass);
-        }
-
-        $serviceClass = config('larate.service.RUB');
-
-        return new self(new $serviceClass);
+        return new self( new $serviceClass,$cache ?? app()->make('cache.store') );
     }
 
     /**
      * Constructor.
      *
      * @param ExchangeRateServiceContract $service
+     * @param CacheInterface $cache
      */
-    public function __construct(ExchangeRateServiceContract $service)
+    public function __construct(ExchangeRateServiceContract $service, CacheInterface $cache)
     {
         $this->service = $service;
+        $this->cache = $cache;
     }
 
     public function getProviderName(): string
@@ -49,20 +51,22 @@ class Larate implements ExchangeRateProviderContract
 
     public function getExchangeRate(CurrencyPairContract $currencyPair, DateTimeInterface $date = null): ExchangeRateContract
     {
+        $dateRate = $date ?? Carbon::now();
+
         if ($currencyPair->isIdentical()) {
-            return new ExchangeRate($currencyPair, 1.0, Carbon::now(), 'null');
+            return new ExchangeRate($currencyPair, 1.0, $dateRate, 'null');
         }
 
-        $cacheKey = CacheHelper::buildCacheKey($currencyPair, $date ?? Carbon::now());
+        $cacheKey = CacheHelper::buildCacheKey($currencyPair, $dateRate);
 
-        $value = Cache::get($cacheKey);
+        $value = $this->cache->get($cacheKey);
 
         if ($value !== null) {
-            return new ExchangeRate($currencyPair, (float) $value, $date, 'cache');
+            return new ExchangeRate($currencyPair, (float) $value, $dateRate, 'cache');
         }
 
-        $rate = $this->service->getExchangeRate($currencyPair, $date);
-        Cache::put($cacheKey, $rate->getValue());
+        $rate = $this->service->getExchangeRate($currencyPair, $dateRate);
+        $this->cache->set($cacheKey, $rate->getValue());
 
         return $rate;
     }
